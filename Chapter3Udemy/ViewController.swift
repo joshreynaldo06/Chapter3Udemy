3d04ce4b0b6abb86ce14d350bd4dd7d79f81158c
Joshua Ronaldo <joshuaronaldo088@gmail.com>
2020-01-21 11:13:13 +0700

    first commit

---
+++ Chapter3Udemy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chapter3Udemy
+//
+//  Created by Joshua Citra on 21/01/20.
+//  Copyright © 2020 Joshua-Citra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
